4b9460f057a4b7a0f76c3ee6787ceacfd8ec1a6b
Govind Hembram <33415980+Ghembram@users.noreply.github.com>
2022-12-01 18:04:49 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,6 +1,8 @@
 const counterReducer = (state,action) =>{
-  return {
-  type:"INC"
+  switch(action.type){
+    case "INC": return state+1;
+    case "DEC": return state-1;
+    default: return state;
   }
   
 
